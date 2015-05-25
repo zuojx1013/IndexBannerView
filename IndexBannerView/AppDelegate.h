09942e60e6c488a8a02de162fac9551400250aab
mac <mac@macdeMac-mini.local>
2015-05-25 16:16:26 +0800

    Initial Commit

---
+++ IndexBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IndexBannerView
+//
+//  Created by mac on 15/5/25.
+//  Copyright (c) 2015年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IndexBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndexBannerView
+//
+//  Created by mac on 15/5/25.
+//  Copyright (c) 2015年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
